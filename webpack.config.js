ca84f7d22ef8115d29eaaa1bfe05627e9f2d139b
Sam-Yi <ghosttesla@gmail.com>
2016-10-22 19:43:37 -0400

    Webpack Config File

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+		devtool: 'inline-source-map',
+		entry: [
+				'webpack-dev-server/client?http://127.0.0.1:8080/', // these two addresses is for hot reloading.
+				'webpack/hot/only-dev-server',
+				'./src'     // since there is no file name, webpack will automatically look for an index.js file.
+		],
+		output: {
+				path:  path.join(__dirname, 'public'),
+				filename: 'bundle.js'
+		},
+		resolve:  { // this is where webpack is going to be looking for the files to bundle together
+				modulesDirectories: ['node_modules', 'src'],
+				extensions: ['', '.js']
+		},
+		module: {
+				loaders: [
+				{
+						test: /\.jsx?$/,
+						exclude: /node_module/,
+						loader: ['react-hot', 'babel?presets[]=react, presets[]=es2015']
+				}
+				]
+		},
+		plugins: [
+				new webpack.HotModuleReplacementPlugin(),
+				new webpack.NoErrorsPlugin()
+		]
+
+};
